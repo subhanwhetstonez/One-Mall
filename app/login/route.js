d0a293249b87e99ec9302f6adf7517a473a1d643
Subhan <subhan.suleman@whetstonez.com>
2024-08-26 18:11:55 +0500

    Delete app/login/route.js

--- app/login/route.js
+++
@@ -1,9 +0,0 @@
-// app/login/route.js
-
-import { NextResponse } from "next/server";
-import User from "../../models/userModel"; // Adjust the path based on your structure
-
-export async function GET(req) {
-  // Optional: Handle GET requests if needed, e.g., to check session status
-  return NextResponse.json({ message: "Login endpoint" }, { status: 200 });
-}
